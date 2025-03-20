91d5f8b64996398760228f701bd8f530eb01b400
manojbishwakarma <manojbishwakarma@neebal.com>
2025-03-20 17:36:48 -0400

    Add MongoDB connection configuration

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 /**
  * File: server.js
- * Student's Name: Manoj Bishwakarma
- * StudentID: 200544391
+ * Student's Name: Manoj Bishwakarma, Laxman Rokaya
+ * StudentID: 200544391, 200544400
  * Date: March 20, 2024
  */
 
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+// MongoDB Connection
+mongoose
+  .connect(
+    "mongodb+srv://manojbishwakarma88:manoj123@recipesdatabase.iogvl.mongodb.net/recipes_db"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Basic middleware
 app.use(express.json());
