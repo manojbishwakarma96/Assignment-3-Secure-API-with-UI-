c6ecd482f3c98140cdebebb1b888f2a3fd3d3a5d
manojbishwakarma <manojbishwakarma@neebal.com>
2025-03-20 17:45:15 -0400

    added recipe model schema to recipeModel.js

---
+++ models/recipeModel.js
@@ -0,0 +1,59 @@
+/**
+ * File: recipeModel.js
+ * Student's Name: Manoj Bishwakarma, Laxman Rokaya
+ * StudentID: 200544391, 200544400
+ * Date: March 20, 2024
+ */
+
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema(
+  {
+    recipeName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ingredients: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    cookingTime: {
+      type: Number,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+      enum: ["Easy", "Medium", "Hard"],
+    },
+    cuisine: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photoLink: {
+      type: String,
+      required: true,
+    },
+    averageRating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Export the model with the name 'recipeModel'
+module.exports = mongoose.model("recipeModel", recipeSchema);
